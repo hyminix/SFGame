3cd485290f675dd95f527706859edd45c3cb6bc0
hyminix <y.arrighi@gmail.com>
2018-05-14 19:28:09 +0200

    Design HTML 5 + Css
    Debut de structuration de l'interface web

--- vuebase.php
+++ vuebase.php
@@ -7,13 +7,28 @@
   <script src="script.js"></script>
 </head>
 <body>
-  <header>
-    <nav id="navigationVue">
-      <a href="#">Vue Monde</a>
-      <a href="#">Vue Base</a>
-      <a href="#">Vue Recherche</a>
-    </nav>
-  </header>
+  <div id="main-content">
+    <header>
+      <nav>
+        <ul id="menu-navigation">
+          <li><a href="#">Base</a></li>
+          <li><a href="#">Production</a></li>
+          <li><a href="#">Modules</a></li>
+          <li><a href="#">Carte</a></li>
+          <li><a href="#">Flotte</a></li>
+          <li><a href="#">Missions</a></li>
+        </ul>
+      </nav>
+    </header>
+    <section>
+      <div id="menu-information-production">a</div>
+    </section>
+    <section>
+      <div id="container-base">a</div>
+    </section>
+
+  </div>
+
 
 </body>
 </html>

adb3de3c76abf704efc54118954638b283faed9c
hyminix <31187490+hyminix@users.noreply.github.com>
2018-05-17 17:46:22 +0200

    mise en place de la base du controlleur

--- vuebase.php
+++ vuebase.php
@@ -11,29 +11,32 @@
     <header>
       <nav>
         <ul id="menu-navigation">
-          <li class="menu-navigation-li"><a href="#">Base</a></li>
-          <li lass="menu-navigation-li"><a href="#">Production</a></li>
-          <li class="menu-navigation-li"><a href="#">Modules</a></li>
-          <li class="menu-navigation-li"><a href="#">Carte</a></li>
-          <li class="menu-navigation-li"><a href="#">Flotte</a></li>
-          <li class="menu-navigation-li"><a href="#">Missions</a></li>
+          <li><a href="#">Base</a></li>
+          <li><a href="#">Production</a></li>
+          <li><a href="#">Modules</a></li>
+          <li><a href="#">Carte</a></li>
+          <li><a href="#">Flotte</a></li>
+          <li><a href="#">Missions</a></li>
         </ul>
       </nav>
     </header>
-    <section>
-      <div>
-        <ul id="menu-information-production">
-          <li>Niveau : 2</li>
-          <li>Flotte : 5</li>
-          <li>Mission Terminées : 3</li>
-          <li>Energie : 124/250</li>
-        </ul>
+    <section id="vueBase">
+      <div id="menuInfoJoueur">
+        <div id="niveauJoueur">Niveau : 2</div>
+        <div id="flotteJoueur">Flotte : 5</div>
+        <div id="missionTermineJoueur">Mission terminé : 20</div>
+        <div id="energieJoueur">Energie : 124/254</div>
       </div>
     </section>
     <section>
       <div id="container-base">a</div>
     </section>
-
+    <section>
+      <div id="menuInteractionBase">
+        <div id="imageBatiment">Image du batiment</div>
+        <div id="affichageInteractionBatiment">- informations sur le bâtiment sélectionné<br /><br />+ possibilité d'ajouter un bâtiment sur un emplacement de base vide<br /><br />+ possibilité de lancer l'upgrade du bâtiment</div>
+      </div>
+    </section>
   </div>
 
 
